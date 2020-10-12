f982d390aeeb05bb775134660bbe789268b62132
Brandon <nardnob@users.noreply.github.com>
2020-10-12 16:30:10 -0500

    Add Kryptonite Sword. Also add images for other tools/armor

---
+++ src/main/java/com/kryptonitemod/tools/KryptoniteItemTier.java
@@ -0,0 +1,93 @@
+package com.kryptonitemod.tools;
+
+import com.kryptonitemod.util.RegistryHandler;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum KryptoniteItemTier implements IItemTier {
+    //Harvest Levels
+    // 3 - diamond
+    // 2 - iron
+    // 1 - stone
+    // 0 - wood
+
+    //Uses
+    // 32   - gold
+    // 59   - wood
+    // 64   - flint & steel
+    // 131  - stone
+    // 250  - iron
+    // 336  - shield
+    // 384  - bow
+    // 432  - elytra
+    // 1561 - diamond
+
+    //Efficiency
+    // 2  - wood
+    // 4  - stone
+    // 6  - iron
+    // 8  - diamond
+    // 10 - gold
+
+    //Enchantability
+    // 14 - iron tools
+    // 9  - iron armor
+    // 22 - gold tools
+    // 25 - gold armor
+    // 10 - diamond tools
+    // 10 - diamond armor
+
+    //Damage
+    //1 (all items do 1 damage) + base damage (3.0F here) + items damage
+
+    //harvest level, uses, efficiency, base attack damage, enchantability
+    KRYPTONITE(3, 1561, 10.0F, 3.0F, 25, () -> { return Ingredient.fromItems(RegistryHandler.KRYPTONITE_SWORD_ITEM.get()); });
+
+    private final int HarvestLevel;
+    private final int MaxUses;
+    private final float Efficiency;
+    private final float AttackDamage;
+    private final int Enchantability;
+    private final Supplier<Ingredient> RepairMaterial;
+
+    KryptoniteItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        HarvestLevel = harvestLevel;
+        MaxUses = maxUses;
+        Efficiency = efficiency;
+        AttackDamage = attackDamage;
+        Enchantability = enchantability;
+        RepairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return MaxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return Efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return AttackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return HarvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return Enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return RepairMaterial.get();
+    }
+}
--- src/main/java/com/kryptonitemod/util/RegistryHandler.java
+++ src/main/java/com/kryptonitemod/util/RegistryHandler.java
@@ -6,8 +6,10 @@ import com.kryptonitemod.blocks.KryptoniteOreBlock;
 import com.kryptonitemod.items.KryptoniteBlockItem;
 import com.kryptonitemod.items.KryptoniteItem;
 import com.kryptonitemod.items.KryptoniteOreItem;
+import com.kryptonitemod.tools.KryptoniteItemTier;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -23,11 +25,21 @@ public class RegistryHandler {
 
     //Items
     public static final RegistryObject<Item> KRYPTONITE_ITEM = Items.register(KryptoniteItem.Name, KryptoniteItem::new);
+
+    //BlockItems
     public static final RegistryObject<Item> KRYPTONITE_ORE_ITEM = Items.register(KryptoniteOreItem.Name, () -> new KryptoniteOreItem(KRYPTONITE_ORE_BLOCK.get()));
     public static final RegistryObject<Item> KRYPTONITE_BLOCK_ITEM = Items.register(KryptoniteBlockItem.Name, () -> new KryptoniteBlockItem(KRYPTONITE_BLOCK.get()));
 
-    public static void init()
-    {
+    //Tools
+    public static final RegistryObject<SwordItem> KRYPTONITE_SWORD_ITEM = Items.register("kryptonite_sword_item", () ->
+            new SwordItem(
+                    KryptoniteItemTier.KRYPTONITE,
+                    6, //1 (all items do 1 damage) + base damage (3.0F here) + items damage (6)
+                    -2.4F, //4 - 2.4 = 1.6 (vanilla iron sword cooldown)
+                    new Item.Properties().group(KryptoniteMod.CreativeTab)
+            ));
+
+    public static void init() {
         Items.register(FMLJavaModLoadingContext.get().getModEventBus());
         Blocks.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
