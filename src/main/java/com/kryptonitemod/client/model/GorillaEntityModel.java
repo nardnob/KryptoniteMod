db2a4ab971e1ddc53dccbbfbea992ebfb6a7d290
Brandon <nardnob@users.noreply.github.com>
2020-10-17 05:38:40 -0500

    Fix GorillaEntity model. Still need to create the texture.

--- src/main/java/com/kryptonitemod/client/model/GorillaEntityModel.java
+++ src/main/java/com/kryptonitemod/client/model/GorillaEntityModel.java
@@ -4,7 +4,6 @@ import com.kryptonitemod.entities.GorillaEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.entity.model.QuadrupedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
 
@@ -24,57 +23,57 @@ public class GorillaEntityModel<T extends GorillaEntity> extends EntityModel<T> 
         textureHeight = 64;
 
         butt_left = new ModelRenderer(this);
-        butt_left.setRotationPoint(-0.5F, 12.4485F, -7.7493F);
-        setRotationAngle(butt_left, -0.9163F, 0.0F, 0.0F);
-        butt_left.setTextureOffset(12, 35).addBox(-3.5F, -0.7608F, -0.5982F, 3.0F, 1.0F, 1.0F, 0.0F, false);
-        butt_left.setTextureOffset(43, 34).addBox(-4.5F, 0.2392F, -0.5982F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-        butt_left.setTextureOffset(27, 2).addBox(-3.5F, 2.2392F, -0.5982F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+        butt_left.setRotationPoint(-0.5F, 12.4485F, 7.7493F);
+        setRotationAngle(butt_left, 0.9163F, 0.0F, 0.0F);
+        butt_left.setTextureOffset(12, 35).addBox(-3.5F, -0.7608F, -0.4018F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+        butt_left.setTextureOffset(43, 34).addBox(-4.5F, 0.2392F, -0.4018F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+        butt_left.setTextureOffset(27, 2).addBox(-3.5F, 2.2392F, -0.4018F, 3.0F, 1.0F, 1.0F, 0.0F, false);
 
         butt_right = new ModelRenderer(this);
-        butt_right.setRotationPoint(-0.5F, 13.4485F, -8.7493F);
-        setRotationAngle(butt_right, -0.9163F, 0.0F, 0.0F);
-        butt_right.setTextureOffset(12, 33).addBox(1.5F, -2.1629F, -0.7828F, 3.0F, 1.0F, 1.0F, 0.0F, false);
-        butt_right.setTextureOffset(28, 40).addBox(0.5F, -1.1629F, -0.7828F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-        butt_right.setTextureOffset(27, 0).addBox(1.5F, 0.8371F, -0.7828F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+        butt_right.setRotationPoint(-0.5F, 13.4485F, 8.7493F);
+        setRotationAngle(butt_right, 0.9163F, 0.0F, 0.0F);
+        butt_right.setTextureOffset(12, 33).addBox(1.5F, -2.1629F, -0.2172F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+        butt_right.setTextureOffset(28, 40).addBox(0.5F, -1.1629F, -0.2172F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+        butt_right.setTextureOffset(27, 0).addBox(1.5F, 0.8371F, -0.2172F, 3.0F, 1.0F, 1.0F, 0.0F, false);
 
         neck = new ModelRenderer(this);
-        neck.setRotationPoint(0.0F, 9.5F, 1.0F);
-        setRotationAngle(neck, -0.6981F, 0.0F, 0.0F);
-        neck.setTextureOffset(43, 27).addBox(-2.0F, -3.5F, -0.5F, 4.0F, 4.0F, 3.0F, 0.0F, false);
+        neck.setRotationPoint(0.0F, 9.5F, -1.0F);
+        setRotationAngle(neck, 0.6981F, 0.0F, 0.0F);
+        neck.setTextureOffset(43, 27).addBox(-2.0F, -3.5F, -2.5F, 4.0F, 4.0F, 3.0F, 0.0F, false);
 
         body = new ModelRenderer(this);
-        body.setRotationPoint(0.0F, 15.25F, -2.0F);
-        setRotationAngle(body, -0.9163F, 0.0F, 0.0F);
-        body.setTextureOffset(0, 0).addBox(-5.0F, -6.9644F, -5.5321F, 10.0F, 13.0F, 7.0F, 0.0F, false);
+        body.setRotationPoint(0.0F, 15.25F, 2.0F);
+        setRotationAngle(body, 0.9163F, 0.0F, 0.0F);
+        body.setTextureOffset(0, 0).addBox(-5.0F, -6.9644F, -1.4679F, 10.0F, 13.0F, 7.0F, 0.0F, false);
 
         head = new ModelRenderer(this);
-        head.setRotationPoint(0.0F, 7.5F, 2.5F);
+        head.setRotationPoint(0.0F, 7.5F, -2.5F);
         head.setTextureOffset(0, 20).addBox(-4.0F, -5.5F, -3.0F, 8.0F, 7.0F, 6.0F, 0.0F, false);
 
         right_arm = new ModelRenderer(this);
-        right_arm.setRotationPoint(-8.0F, 14.5F, 1.0F);
-        right_arm.setTextureOffset(34, 0).addBox(-1.0F, -7.5F, -3.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
-        right_arm.setTextureOffset(23, 28).addBox(-2.0F, 2.5F, -3.0F, 5.0F, 7.0F, 5.0F, 0.0F, false);
+        right_arm.setRotationPoint(-8.0F, 14.5F, -1.0F);
+        right_arm.setTextureOffset(34, 0).addBox(-1.0F, -7.5F, -1.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
+        right_arm.setTextureOffset(23, 28).addBox(-2.0F, 2.5F, -2.0F, 5.0F, 7.0F, 5.0F, 0.0F, false);
 
         left_arm = new ModelRenderer(this);
-        left_arm.setRotationPoint(8.0F, 14.5F, 1.0F);
-        left_arm.setTextureOffset(0, 33).addBox(-3.0F, -7.5F, -3.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
-        left_arm.setTextureOffset(29, 15).addBox(-3.0F, 2.5F, -3.0F, 5.0F, 7.0F, 5.0F, 0.0F, false);
+        left_arm.setRotationPoint(8.0F, 14.5F, -1.0F);
+        left_arm.setTextureOffset(0, 33).addBox(-3.0F, -7.5F, -1.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
+        left_arm.setTextureOffset(29, 15).addBox(-3.0F, 2.5F, -2.0F, 5.0F, 7.0F, 5.0F, 0.0F, false);
 
         left_leg = new ModelRenderer(this);
-        left_leg.setRotationPoint(4.0F, 18.5F, -4.0F);
-        left_leg.setTextureOffset(16, 40).addBox(-3.0F, -1.5F, -4.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
+        left_leg.setRotationPoint(4.0F, 18.5F, 4.0F);
+        left_leg.setTextureOffset(16, 40).addBox(-3.0F, -1.5F, 0.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
 
         right_leg = new ModelRenderer(this);
-        right_leg.setRotationPoint(-4.0F, 20.5F, -4.0F);
-        right_leg.setTextureOffset(39, 39).addBox(-1.0F, -3.5F, -4.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
+        right_leg.setRotationPoint(-4.0F, 20.5F, 4.0F);
+        right_leg.setTextureOffset(39, 39).addBox(-1.0F, -3.5F, 0.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
     }
 
     @Override
     public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.head.rotateAngleX = headPitch * ((float)Math.PI / 180F);
         this.head.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
-        this.body.rotateAngleX = ((float)Math.PI / 2F);
+        //this.body.rotateAngleX = ((float)Math.PI / 2F);
         this.right_leg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
         this.left_leg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
         this.right_arm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
--- src/main/java/com/kryptonitemod/entities/GorillaEntity.java
+++ src/main/java/com/kryptonitemod/entities/GorillaEntity.java
@@ -19,14 +19,12 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class GorillaEntity extends AnimalEntity {
     public static final Ingredient temptationItems = Ingredient.fromItems(Items.BEETROOT);
 
     private EatGrassGoal _eatGrassGoal;
-    private int _eatGrassTimer;
+    private static final double _movementSpeed = 0.5;
 
     public GorillaEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
         super(type, worldIn);
@@ -36,7 +34,7 @@ public class GorillaEntity extends AnimalEntity {
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
         return MobEntity.func_233666_p_() //registerAttributes()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 20.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.5D);
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, _movementSpeed);
     }
 
     @Override
@@ -44,12 +42,12 @@ public class GorillaEntity extends AnimalEntity {
         super.registerGoals();
         this._eatGrassGoal = new EatGrassGoal(this);
         this.goalSelector.addGoal(0, new SwimGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
-        this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.1D, this.temptationItems, false));
-        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(1, new PanicGoal(this, _movementSpeed));
+        this.goalSelector.addGoal(2, new BreedGoal(this, _movementSpeed));
+        this.goalSelector.addGoal(3, new TemptGoal(this, _movementSpeed, temptationItems, false));
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, _movementSpeed));
         this.goalSelector.addGoal(5, this._eatGrassGoal);
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, _movementSpeed));
         this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
     }
@@ -77,30 +75,13 @@ public class GorillaEntity extends AnimalEntity {
     @Nullable
     @Override
     public AgeableEntity createChild(AgeableEntity ageable) {
-
         return KryptoniteEntityTypes.gorillaEntity.get().create(this.world);
-    }
-
-    @Override
-    protected void updateAITasks() {
-        this._eatGrassTimer = this._eatGrassGoal.getEatingGrassTimer();
-        super.updateAITasks();
     }
 
     @Override
     public void livingTick() {
         if (this.world.isRemote) {
-            this._eatGrassTimer = Math.max(0, this._eatGrassTimer - 1);
         }
         super.livingTick();
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public void handleStatusUpdate(byte id) {
-        if (id == 10) {
-            this._eatGrassTimer = 40;
-        } else {
-            super.handleStatusUpdate(id);
-        }
     }
 }
